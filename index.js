6b2faf60e9c654f5bb7612e26776cb42c235034f
benjaminhonorio <benjaminhonorio.12@gmail.com>
2021-12-19 18:25:59 -0500

    ready for deployment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -33,6 +34,7 @@ const generateId = () => {
   return Math.floor(Math.random() * 100000000);
 };
 
+app.use(cors())
 app.use(express.json());
 
 // Don't do this.. just for demo purposes
